d5c93b6f752767aa4154bf5eda47fce0010c5554
meriam <meriam.hamdaoui@gmail.com>
2025-05-12 22:15:30 +0100

    reorganize

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function generateQRCode() {
+  var showMe = document.getElementById("showText").value;
+
+  alert(showMe);
+
+  // var message = document.getElementById("showMsg");
+  // message.innerHTML = showMe;
+}
