c0019639d84aaee08e77435a15e7017770481daa
meriam <meriam.hamdaoui@gmail.com>
2025-05-13 12:08:51 +0100

    qr code generate works

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-function generateQRCode() {
-  var showMe = document.getElementById("showText").value;
+function qrAPI(txt, size) {
+  return `https://quickchart.io/chart?cht=qr&chs=${size}&chl=${txt}`;
+}
 
-  alert(showMe);
+function generateQRCode() {
+  var qrMsg = document.getElementById("QRtxt").value;
+
+  if (qrMsg === "") {
+    alert("Please enter a message to generate a QR code.");
+    return;
+  }
+
+  var qrSize = document.getElementById("size").value;
+
+  var qrCode = document.getElementById("qrCode");
+  qrCode.src = qrAPI(qrMsg, qrSize);
+  qrCode.style.display = "block";
 
   // var message = document.getElementById("showMsg");
   // message.innerHTML = showMe;
+}
+function showQR() {
+  var showMe = document.getElementById("QRtxt").value;
+  var qrCode = document.getElementById("qrCode");
+  qrCode.src = qrAPI(showMe, 300);
+  qrCode.style.display = "block";
 }
